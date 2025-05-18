bb9a375a4c90756c0f863d2f91a9f70c0ca6614b
Anusha <anushatest199126@gmail.com>
2025-05-18 08:24:27 -0400

    remove

--- src/TestDemo/RemoveDuplicateArray.java
+++ src/TestDemo/RemoveDuplicateArray.java
@@ -19,19 +19,7 @@ public class RemoveDuplicateArray {
      
      System.out.print(set);
      
- 	int n= 10;
-	
-	 for(int j=2;j<=n;j++){
-       int counter=0;
-        for(int i=1;i<=j;i++){
-           if(j%i==0){
-              counter++;
-           }
-        }
-        if(counter==2)
-        System.out.print(j+" ");
-     }// TODO Auto-generated method stub
-
+ 	
 
 	}
 
